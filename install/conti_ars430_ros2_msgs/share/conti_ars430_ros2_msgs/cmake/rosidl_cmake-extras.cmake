b498d38762992f2a094a2f0926594a62e3e82e59
mhb0036 <mhb0036@auburn.edu>
2025-10-13 13:49:59 -0500

    Delete install directory

--- install/conti_ars430_ros2_msgs/share/conti_ars430_ros2_msgs/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(conti_ars430_ros2_msgs_IDL_FILES "msg/SensorStatus.idl;msg/RadarDetection.idl;msg/RadarDetectionImage.idl")
-set(conti_ars430_ros2_msgs_INTERFACE_FILES "msg/SensorStatus.msg;msg/RadarDetection.msg;msg/RadarDetectionImage.msg")
